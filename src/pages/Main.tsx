fe095568968b8aeed0c4191244e23fd4329d6da4
shubhraagarwal27 <shubhra@metawork.team>
2023-02-14 17:09:39 +0530

    modified icons

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,12 @@
+import Nav from "@/components/nav/Nav";
 import React from "react";
 
 function Main() {
-  return <div>Main</div>;
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
 }
 
 export default Main;
