1bc1c436510e7803e0c258fcd0ca5444ffed2151
Aditya <aditya26sg@gmail.com>
2023-02-15 01:52:11 +0530

    getting data wip

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,25 +1,21 @@
-import { useWalletConnect } from "@walletconnect/react-native-dapp";
 import { ethers } from "ethers";
 import { createContext, useEffect, useMemo, useState } from "react";
 import { logger } from "../helpers/logger";
 import useLensAuth from "../hooks/lens/useLensAuth";
 import useStreamClient from "../hooks/stream/useStreamClient";
 import { UserDb$ } from "../schema/user";
-import { removeAsyncData } from "../service/AsyncStorageService";
+// import { removeAsyncData } from "../service/AsyncStorageService";
 import { putStreamToken } from "../service/StreamService";
 import { findOrCreateUser } from "../service/UserService";
-
 
 export type AuthContextType = {
     signer: any | undefined;
     address: any | undefined;
     connectWallet: () => void;
     disconnectWallet: () => void;
-    connector: any;
     user: any | undefined;
-    setUser: (param) => void;
-    updateUser: (...params) => void;
-    testingFunction: () => void;
+    setUser: (param: any) => void;
+    updateUser: (...params: any) => void;
     streamClient: any | undefined;
     connectLens: () => void;
     authToken: any | undefined;
@@ -30,41 +26,23 @@ export const AuthContext = createContext<AuthContextType>({
     address: "",
     connectWallet: () => {},
     disconnectWallet: () => {},
-    connector: null,
     user: null,
     setUser: (param) => {},
     updateUser: (...params) => {},
-    testingFunction: () => {},
     streamClient: null,
     connectLens: () => {},
     authToken: null,
 });
 
-const AuthProvider = ({children}) => {
-    const connector = useWalletConnect();
+const AuthProvider = ({children}: any) => {
     const [signer, setSigner] = useState<any>("");
     const [address, setAddress] = useState<any>();
     const [user, setUser] = useState<any>();
     const [streamClient, setStreamClient] = useState<any>();
 
-
-    // const provider = new ethers.providers.JsonRpcProvider(
-    //   "https://polygon-mainnet.g.alchemy.com/v2/mVc4fkR9Ie98VuFA0EeRgGVM3LYLBfHh",
-    //   "matic"
-    // );
-
-    // const provider = new ethers.providers.JsonRpcProvider(
-    //   "https://polygon-mumbai.g.alchemy.com/v2/rOwXJQfWxqJtdoT9jlVFnj-g_M4nuu8J",
-    //   "maticmum"
-    // )
-
-    const provider = new ethers.providers.JsonRpcProvider(
-        "https://rpc-mumbai.maticvigil.com"
-    );
-
-    const updateUser = (key, data) => {
+    const updateUser = (key: any, data: any) => {
         logger('auth', 'updateUser', "Updating user data", [key, data]);
-        const newData = {};
+        const newData: any = {};
         newData[key] = data;
         setUser({...user, ...newData});
     };
@@ -76,25 +54,12 @@ const AuthProvider = ({children}) => {
     // connecting user wallet
     const connectWallet = async () => {
         logger('auth', 'connectWallet', 'Trigger connectWallet', []);
-        const promise = new Promise(async (resolve, reject) => {
-            if (connector && !connector.connected) {
-                resolve(await connector.connect());
-                logger('auth', 'connectWallet','Wallet is not connected', [connector.accounts]);
-            } else {
-                resolve(null);
-                logger('auth', 'connectWallet','Wallet is connected', [connector.accounts]);
-            }
-            //TODO: resolving promise issue
-        });
-        return promise;
+        
     };
 
     // Disconnecting user Wallet
     const disconnectWallet = async () => {
-        logger('auth', 'disconnectWallet', 'Trigger disconnectWallet', []);
-        removeAsyncData("accessToken");
-        await connector.killSession();
-        resetToDefault();        
+               
     };
 
     const resetToDefault = () => {
@@ -104,38 +69,16 @@ const AuthProvider = ({children}) => {
         hookLensAuth.setLensProfile(null);
     };
 
-    const testingFunction = async () => {
-        hookStreamClient.connectToStream();
-    };
-
     const connectLens = () => {
         hookLensAuth.connect();
     };
 
     // setting the address in context
-    useEffect(() => {
-        logger('auth', 'useEffect[connect.accounts]', 'run', [])
-        if (connector.connected) {
-            logger('auth', 'useEffect[connect.accounts]', 'connector is connected', [ provider.connection])
-            setAddress(connector.accounts[0]);
-            console.log("Re-connecting address ", connector.accounts[0]);
-            const balance = provider.getBalance(connector.accounts[0]);
-            const signer = provider.getSigner(connector.accounts[0]);
-            setSigner(signer);
-        }
-    }, [connector.accounts]);
+    
 
     useEffect(() => {
         logger('auth', 'useEffect[user]', 'User is', [user])
     }, [user]);
-
-    // fetch the lens account when the wallet is connected
-
-    useEffect(() => {
-        if (address) {
-            logger('auth', 'useEffect[address]', "Address is ", [address]);
-        }
-    }, [address])
 
     useMemo(() => {
         logger('auth', 'useMemo[user.lens]', 'User is',  [user])
@@ -143,30 +86,17 @@ const AuthProvider = ({children}) => {
             logger('auth', 'useMemo[user.lens]','requesting user from DB with address', [address])
             findOrCreateUser({
                 address: address.toLowerCase(),
-            }).then((data) => {
+            }).then((data: any) => {
                 logger('auth', 'useMemo[user.lens]', 'response of user from DB', [JSON.stringify(data)]);
                 updateUser("db", UserDb$(data));
             });
         }
     }, [user?.lens])
 
-    // useEffect(() => {
-    //     console.log("User profile in AuthProvider ", user);
-    //     if (address) {
-    //         console.log("calling the findorCreateUser service function");
-    //         findOrCreateUser({
-    //             address: address.toLowerCase(),
-    //         }).then((data) => {
-    //             console.log("Find or create user", JSON.stringify(data));
-    //             updateUser("db", UserDb$(data));
-    //         });
-    //     }
-    // }, [address]);
-
     useEffect(() => {
         logger('auth', 'useEffect[user.db.id]', 'run', [])
         if (user?.db?.id && !user?.db?.tokens?.stream) {
-            putStreamToken({userAddress: address}).then((res) => {
+            putStreamToken({userAddress: address}).then((res: any) => {
                 logger('auth', 'useEffect[user.db.id]', 'response from putStreamToken api', [res])
                 updateUser("db", UserDb$(res));
             });
@@ -181,11 +111,9 @@ const AuthProvider = ({children}) => {
                 address,
                 connectWallet,
                 disconnectWallet,
-                connector,
                 user,
                 setUser,
                 updateUser,
-                testingFunction,
                 streamClient,
                 connectLens,
                 authToken,
---
+++ src/service/StreamService.tsx
@@ -0,0 +1,72 @@
+import { config } from "../config";
+
+export const putStreamToken = async(body: any) => {
+    const response = await fetch(`${config.url}/api/stream/token/create`, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+    });
+    const data = await response.json();
+    return data;
+}
+
+
+export const postStreamChannel = async(data: any) => {
+    const response = await fetch(`${config.url}/api/stream/channel/update`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+    });
+}
+
+export const addStreamMembers = async (data: any) => {
+    const response = await fetch (`${config.url}/api/stream/addmember`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+    return response;
+}
+
+export const removeStreamMembers = async (data: any) => {
+    const response = await fetch (`${config.url}/api/stream/removemember`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+    return response;
+}
+
+export const viewStreamMembers = async (channelId: string) => {
+    const response = await fetch (`${config.url}/api/stream/viewmembers/${channelId}`);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+export const listenPresence = async(channelId: string) => {
+    const response = await fetch(`${config.url}/api/channels/presence/${channelId}`);
+}
+
+//creating a relation between channel and member added to stream
+export const addChannelMembers = async(data: any) => {
+    console.log("Channel add data", data);
+    const response = await fetch(`${config.url}/api/channel/member/add`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+}
+
