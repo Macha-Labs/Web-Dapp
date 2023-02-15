a896746e57c745d884408be922f6191ec2b2864f
Saksham <saksham@metawork.team>
2023-02-15 18:23:39 +0530

    minor

--- src/components/chat/chatcontainer/ChatHeader.tsx
+++ src/components/chat/chatcontainer/ChatHeader.tsx
@@ -1,9 +1,25 @@
+import ModalSlider from "@/components/modal/ModalSlider";
 import { Row } from "@/styles/StyledComponents";
-import { Heading, Icon } from "@chakra-ui/react";
+import { Heading, Icon, useDisclosure } from "@chakra-ui/react";
 
 const ChatHeader = props => {
+  const membersModal = useDisclosure();
+
+  const templateMembers = () => {
+    <ModalSlider event={membersModal}></ModalSlider>
+  }
+
+  const templatePinnedMessages = () => {
+
+  }
+
+  const templateProfile = () => {
+
+  }
+
   return (
-    <div className="header hr-between vr-center">
+    <>
+      <div className="header hr-between vr-center">
       <Row className="w-100 h-100 hr-between vr-center">
         <Heading as="h4" size="sm">
           #{props?.hookChannel?.channel?.name}
@@ -16,13 +32,15 @@ const ChatHeader = props => {
             <Icon className="m-r-0-5">
               {/* <SearchIcon width="25" height="25" fill="#efefef"></SearchIcon>/ */}
             </Icon>
-            <Icon className="" onClick={() => {}}>
+            <Icon className="" onClick={membersModal.onOpen}>
               {/* <UserIcon width="25" height="25" fill="#efefef"></UserIcon> */}
             </Icon>
           </Row>
         </div>
       </Row>
     </div>
+    {templateMembers()}
+    </>
   );
 };
 
---
+++ src/components/modal/ModalSlider.tsx
@@ -0,0 +1,44 @@
+import {
+    Avatar,
+    Button,
+    Heading,
+    Drawer,
+    DrawerBody,
+    DrawerCloseButton,
+    DrawerContent,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+} from '@chakra-ui/react';
+import { Row } from '@/styles/StyledComponents';
+
+const ModalSlider = (props) => {
+
+
+    return (
+        <Drawer
+            isOpen={props.event.isOpen}
+            onClose={props.event.onClose}
+            size="xs"
+        >
+            <DrawerOverlay />
+            <DrawerContent>
+                
+            <DrawerHeader>
+                <Row className="vr-center">
+                    <Row className="vr-center">
+                        <Heading as="h5" fontSize={18}>{props.header.title}</Heading>
+                    </Row>
+                    
+                    <DrawerCloseButton />
+                </Row>
+            </DrawerHeader>
+                <DrawerBody>
+                        {props.children}
+                </DrawerBody>
+            </DrawerContent>
+        </Drawer>
+    )
+}
+
+export default ModalSlider;
