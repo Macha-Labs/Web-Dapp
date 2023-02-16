a339e56400f976edcc0b7bb0147e2b3dd3e38c56
Saksham <saksham@metawork.team>
2023-02-16 18:54:14 +0530

    minor

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,18 +1,25 @@
 import ChatList from "@/components/chat/ChatList";
-import { Row, StyledChat, StyledChatList } from "@/styles/StyledComponents";
+import { Row, StyledChat, StyledChatList, StyledWindow } from "@/styles/StyledComponents";
 import React, { useState } from "react";
 import ChatContainer from "@/components/chat/chatcontainer/ChatContainer";
+import Nav from "@/components/nav/Nav";
 
 function Chat() {
   return (
-    <Row>
-      <StyledChatList>
-        <ChatList />
-      </StyledChatList>
-      <StyledChat>
-        <ChatContainer />
-      </StyledChat>
-    </Row>
+      <StyledWindow>
+        <div className="left">
+            <Nav />
+        </div>
+
+        <div className="right">
+          <StyledChatList>
+            <ChatList />
+          </StyledChatList>
+          <StyledChat>
+            <ChatContainer />
+          </StyledChat>
+        </div>
+      </StyledWindow>
   );
 }
 
