6f76d6d81151c2f384aaa8ac01487840e5eef418
Aditya <aditya26sg@gmail.com>
2023-02-17 10:51:13 +0530

    changing stream tokens

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
 export const config = {
     WEB3STORAGE_TOKEN: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDUwMkRmQzQyODIwZWM1RDhFOTEwRjgxYzI3Mjk5NzcxZjk3Q2Q2MjEiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjA4OTk0MzU4NTQsIm5hbWUiOiJUZXN0VG9rZW4ifQ.z3E0zHpfF1xSibdvLC35sH8JY9xf3rYiY_l_MkUgjDM',
-    STREAM_APPID: '1229918',
-    STREAM_APIKEY: 'r56hkgd4zm3n',
-    STREAM_SECRET: 'pxfyrc4mhmufvn4xvyy6meet5p39ay42trwh4yp3q95wttktcn8qsfx6gtpy3qv2',
+    STREAM_APPID: '1235460',
+    STREAM_APIKEY: 'bpm3zjatbqj3',
+    STREAM_SECRET: 'rgvd9g2c3z5mecz2bz9qysncwvrd997d96mupu8bgss7zkumxq2pp82yjyu9yvbk',
     url: 'https://metawork-server.herokuapp.com',
     // url: 'http://192.168.29.165:3080',
     DISCORD_API_ENDPOINT: "https://discord.com/api/v10",
