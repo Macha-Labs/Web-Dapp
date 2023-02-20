cf09b77e5a99ae3b9a05c7cab38bf476ab6e2503
Aditya <aditya26sg@gmail.com>
2023-02-20 18:33:06 +0530

    follow working

--- src/hooks/lens/useLensFollow.ts
+++ src/hooks/lens/useLensFollow.ts
@@ -1,22 +1,22 @@
-import {AuthContext, AuthContextType} from "../../providers/AuthProvider";
-import {ethers} from "ethers";
-import {splitSignature} from "ethers/lib/utils";
-import {followUser, unfollowUser} from "../../helpers/lens/lens";
-import {config} from "../../config";
-import {signedTypeData} from "../../helpers/lens/lensApiService";
-import {useContext, useState} from "react";
-import {lensHubAbi} from "../../abi/lensHubAbi";
-import {lensFollowAbi} from "../../abi/lensFollowAbi";
+import { AuthContext, AuthContextType } from "../../providers/AuthProvider";
+import { ethers } from "ethers";
+import { splitSignature } from "ethers/lib/utils";
+import { followUser, unfollowUser } from "../../helpers/lens/lens";
+import { config } from "../../config";
+import { signedTypeData } from "../../helpers/lens/lensApiService";
+import { useContext, useState } from "react";
+import { lensHubAbi } from "../../abi/lensHubAbi";
+import { lensFollowAbi } from "../../abi/lensFollowAbi";
 import { fetchSigner } from "@wagmi/core";
 
 const useLensFollows = (profileID: any) => {
-  const authContext = useContext(AuthContext) as AuthContextType;
-  const [isFollowing, setIsFollowing] = useState<any>();
-  const [isFollowingByMe, setIsFollowingYou] = useState<any>();
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [loadingText, setLoadingText] = useState<string>("");
+    const authContext = useContext(AuthContext) as AuthContextType;
+    const [isFollowing, setIsFollowing] = useState<any>();
+    const [isFollowingByMe, setIsFollowingYou] = useState<any>();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [loadingText, setLoadingText] = useState<string>("");
 
-  const TEST_LENS_HUB_CONTRACT = "0x60Ae865ee4C725cd04353b5AAb364553f56ceF82";
+    const TEST_LENS_HUB_CONTRACT = "0x60Ae865ee4C725cd04353b5AAb364553f56ceF82";
 
     const getVRS = async (typedData: any) => {
         const signature: any = await signedTypeData(
@@ -25,7 +25,7 @@ const useLensFollows = (profileID: any) => {
             typedData.value,
         );
 
-        const {v, r, s} = splitSignature(signature);
+        const { v, r, s } = splitSignature(signature);
 
         const sig = {
             v,
@@ -37,28 +37,25 @@ const useLensFollows = (profileID: any) => {
         return [typedData, sig];
     };
 
-  const triggerFollow = async () => {
-    if (profileID) {
-      try {
-        setIsLoading(true);
-        setLoadingText("Following");
+    const triggerFollow = async () => {
+        if (profileID) {
+            try {
+                setIsLoading(true);
+                setLoadingText("Following");
 
-        const result = await followUser({
-          follow: [
-            {
-              profile: profileID,
-            },
-          ],
-        });
+                const result = await followUser({
+                    follow: [
+                        {
+                            profile: profileID,
+                        },
+                    ],
+                });
 
                 const [typedData, sig] = await getVRS(
                     result.data!.createFollowTypedData.typedData
                 );
 
                 const signer: any = await fetchSigner();
-
-                console.log("The authsigner is ", signer);
-
                 const lensHub = new ethers.Contract(
                     config.TESTNET_LENS_HUB_CONTRACT,
                     lensHubAbi,
@@ -71,37 +68,37 @@ const useLensFollows = (profileID: any) => {
                     datas: typedData.value.datas,
                     sig: sig,
                 })
-                    
+
                 setIsLoading(false);
                 setLoadingText("Following");
-                
+
                 // await updateLens.updateLensState();
                 return tx.hash;
             } catch (error) {
                 setIsLoading(false);
             }
         } else {
-            throw new Error ("Profile ID to follow is not provided");
+            throw new Error("Profile ID to follow is not provided");
         }
     };
 
-  const triggerUnFollow = async () => {
-    if (profileID) {
-      try {
-        setIsLoading(true);
-        setLoadingText("UnFollowing");
+    const triggerUnFollow = async () => {
+        if (profileID) {
+            try {
+                setIsLoading(true);
+                setLoadingText("UnFollowing");
 
-                const result = await unfollowUser({profile: profileID});
+                const result = await unfollowUser({ profile: profileID });
                 const [typedData, sig] = await getVRS(
                     result.data!.createUnfollowTypedData.typedData
                 );
 
-        // load up the follower nft contract
-        const followNftContract = new ethers.Contract(
-          typedData.domain.verifyingContract,
-          lensFollowAbi,
-          authContext.signer
-        );
+                // load up the follower nft contract
+                const followNftContract = new ethers.Contract(
+                    typedData.domain.verifyingContract,
+                    lensFollowAbi,
+                    authContext.signer
+                );
 
                 const tx = await followNftContract.burnWithSig(
                     typedData.value.tokenId,
@@ -121,14 +118,14 @@ const useLensFollows = (profileID: any) => {
         }
     };
 
-  return {
-    isFollowing: isFollowing,
-    isFollowingByMe: isFollowingByMe,
-    triggerFollow: triggerFollow,
-    triggerUnFollow: triggerUnFollow,
-    isLoading: isLoading,
-    loadingText: loadingText,
-  };
+    return {
+        isFollowing: isFollowing,
+        isFollowingByMe: isFollowingByMe,
+        triggerFollow: triggerFollow,
+        triggerUnFollow: triggerUnFollow,
+        isLoading: isLoading,
+        loadingText: loadingText,
+    };
 };
 
 export default useLensFollows;
