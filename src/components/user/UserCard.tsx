df573dd99083549de1c4eeb80425761b9a8e7e2f
Saksham <saksham@metawork.team>
2023-03-01 12:07:51 +0530

    mior

--- src/components/user/UserCard.tsx
+++ src/components/user/UserCard.tsx
@@ -1,33 +1,11 @@
 import { helperIPFS, truncateAddress } from "@/helpers";
-import { User$ } from "@/schema/user";
 import { Row, StyledCard } from "@/styles/StyledComponents";
 import { Avatar, AvatarBadge, useDisclosure } from "@chakra-ui/react";
-import { useState } from "react";
-import ModalSlider from "../modal/ModalSlider";
-import UserProfile from "./UserProfile";
 
 const UserCard = ({user}) => {
-  const modalProfile = useDisclosure();
-  const [selectedUser, setSelectedUser] = useState<any>();
-
-  const handleSelectedUser = (user: any) => {
-    modalProfile.onOpen();
-    setSelectedUser(user);
-  };
-
-  const TemplateProfile = () => {
-    console.log(selectedUser, "selectedUser");
-    return (
-      <ModalSlider event={modalProfile} size="lg">
-        <UserProfile user={selectedUser} />
-      </ModalSlider>
-    );
-  };
+  
   return (
     <StyledCard
-      onClick={() => {
-        handleSelectedUser(user);
-      }}
       className="border state_hover"
     >
       <Row className="vr-center item m-b-0-5">
@@ -44,7 +22,6 @@ const UserCard = ({user}) => {
           <h6>{truncateAddress(user?.lens?.handle)}</h6>
         )}
       </Row>
-      <TemplateProfile />
     </StyledCard>
   );
 };
