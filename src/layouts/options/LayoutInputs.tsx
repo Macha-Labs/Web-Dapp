63cbbb7f42d9781e16df39579315dc2c084882cc
shubhraagarwal27 <shubhra@metawork.team>
2023-03-01 18:12:16 +0530

    Revert

--- src/layouts/options/LayoutInputs.tsx
+++ src/layouts/options/LayoutInputs.tsx
@@ -1,14 +1,7 @@
-import usePortalChannel from "@/hooks/portal/usePortalChannel";
-import { Channel$ } from "@/schema/channel";
 import { StyledCard } from "@/styles/StyledComponents";
 import { Input } from "@chakra-ui/react";
-import { useState } from "react";
-import { text } from "stream/consumers";
 
 const LayoutInputs = (props: any) => {
-  const [data, setData] = useState("");
-  const hookPortalChannel = usePortalChannel(Channel$({}));
-  console.log("here");
   return (
     <div className={props.style.class}>
       {props.data.map((item: any, index: any) => {
@@ -16,17 +9,8 @@ const LayoutInputs = (props: any) => {
           <Input
             key={index}
             placeholder={item?.label}
-            value={
-              item.key === "name"
-                ? hookPortalChannel?.channel?.name
-                : hookPortalChannel?.channel?.description
-            }
-            onChange={e =>
-              hookPortalChannel?.setChannel({
-                ...hookPortalChannel?.channel,
-                [item.key]: e.target.value,
-              })
-            }
+            value={item?.value}
+            onChange={e => item?.onChange(e.target.value)}
             disabled={item.disabled}
             className="m-b-0-5"
           />
