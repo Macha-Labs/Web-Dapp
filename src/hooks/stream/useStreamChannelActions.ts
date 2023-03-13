edee7498df795b8719e27bdd831ba911c2816237
Aakash <aakash@metawork.team>
2023-03-13 13:06:12 +0530

    members

--- src/hooks/stream/useStreamChannelActions.ts
+++ src/hooks/stream/useStreamChannelActions.ts
@@ -1,9 +1,10 @@
 import { logger } from "@/helpers/logger";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { ChatContext } from "@/providers/ChatProvider";
 
 const useStreamChannelActions = (channel: any) => {
     const [messages, setMessages] = useState<any>();
-
+    const chatContext = useContext(ChatContext);
     useEffect(() => {
         logger("channel", "useEffect[channel]", "channel is ", [channel]);
         if (channel) {
@@ -33,6 +34,24 @@ const useStreamChannelActions = (channel: any) => {
           channel?.raw?.on("reaction.deleted", (event: any) => {
             logger("stream", "useStreamChannel.useEffect", "logging the message reaction events", [event]);
             setMessages(channel?.raw?.state?.messageSets[0]?.messages.slice(0));
+          });
+          channel?.raw?.on("member.added", (event: any) => {
+            logger(
+              "stream",
+              "useStreamChannel.useEffect",
+              "logging added member event",
+              [event]
+            );
+            chatContext?.streamContext.reloadMembers();
+          });
+          channel?.raw?.on("member.removed", (event: any) => {
+            logger(
+              "stream",
+              "useStreamChannel.useEffect",
+              "logging remove member event",
+              [event]
+            );
+            chatContext?.streamContext.reloadMembers();
           });
       }, [channel]);
 
