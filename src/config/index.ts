dfd02a67eee7700622ff470b7f28cac7a36a2cc3
Aditya <aditya26sg@gmail.com>
2023-03-14 15:27:58 +0530

    update mainnet fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,10 @@
 export const config = {
   WEB3STORAGE_TOKEN:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDUwMkRmQzQyODIwZWM1RDhFOTEwRjgxYzI3Mjk5NzcxZjk3Q2Q2MjEiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjA4OTk0MzU4NTQsIm5hbWUiOiJUZXN0VG9rZW4ifQ.z3E0zHpfF1xSibdvLC35sH8JY9xf3rYiY_l_MkUgjDM",
-  STREAM_APPID: "1235460",
-  STREAM_APIKEY: "bpm3zjatbqj3",
+  STREAM_APPID: "1239221",
+  STREAM_APIKEY: "s7k8yarexp9g",
   STREAM_SECRET:
-    "rgvd9g2c3z5mecz2bz9qysncwvrd997d96mupu8bgss7zkumxq2pp82yjyu9yvbk",
+    "czw7rguj3bcpzetr5e2fcx366rq26g38rg6taj9x4gr6h8my3sk25msrx7bcr4we",
   url: "https://metawork-server.herokuapp.com",
   // url: "http://localhost:3080",
   DISCORD_API_ENDPOINT: "https://discord.com/api/v10",
