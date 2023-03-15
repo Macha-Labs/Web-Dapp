bc157d6746b310a097ce662856c84efff6be714a
Saksham <saksham@metawork.team>
2023-03-15 12:02:19 +0530

    minor

---
+++ src/hooks/xmtp/useXmtpChannels.ts
@@ -0,0 +1,31 @@
+import { AuthContext } from "@/providers/AuthProvider";
+import { ChannelXMTP$ } from "@/schema/channel";
+import { useContext, useEffect, useState } from "react";
+
+const useXmtpChannels = () => {
+    const authContext = useContext(AuthContext);
+    const [allConversations, setAllConversations] = useState<any>();
+
+    useEffect(() => {
+        if (authContext?.xmtpClientAddress) {
+          _fetch();
+        }
+      }, [authContext?.xmtpClientAddress]);
+
+    const _fetch = async () => {
+        const conversationList = await authContext?.xmtpClient.conversations.list();
+        const data = conversationList.map((item: any) => {
+          return ChannelXMTP$(item);
+        });
+        setAllConversations(data);
+      };
+
+    return (
+        {
+            fetch: _fetch,
+            channels: allConversations
+        }
+    )
+}
+
+export default useXmtpChannels;
