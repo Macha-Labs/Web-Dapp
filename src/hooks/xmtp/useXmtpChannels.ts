24edc65498ad41cd47dce4d5b9abcc311e9b8e0c
Saksham <saksham@metawork.team>
2023-03-20 12:24:10 +0530

    minor

--- src/hooks/xmtp/useXmtpChannels.ts
+++ src/hooks/xmtp/useXmtpChannels.ts
@@ -1,7 +1,6 @@
 import { logger } from "@/helpers/logger";
 import { AuthContext } from "@/providers/AuthProvider";
 import { Channel$ } from "@/schema/channel";
-import useChatChannelStore from "@/store/useChatChannelStore";
 import { useContext, useEffect, useState } from "react";
 import useLensProfileList from "../lens/useLensProfileList";
 
@@ -10,11 +9,16 @@ const useXmtpChannels = () => {
   const authContext = useContext(AuthContext);
   const [allConversations, setAllConversations] = useState<any>();
   const [rawConversations, setRawConversations] = useState<any>();
-  const [xmtpConvo, setXmtpConvo] = useState<any>();
   const hookLensProfileList = useLensProfileList();
 
-  const _listen = async () => {
-    for await (const conversation of xmtpConvo) {
+  useEffect(() => {
+    if (authContext?.xmtpLogs) {
+      _watch();
+    }
+  }, [authContext?.xmtpLogs]);
+
+  const _watch = async () => {
+    for await (const conversation of authContext?.xmtpLogs) {
       console.log("New conversation started with ", conversation);
       setAllConversations((prevConversations: any) => {
         const conversations = [...prevConversations];
@@ -25,41 +29,13 @@ const useXmtpChannels = () => {
   };
 
   const _unwatch = () => {
-
+    authContext?.xmtpLogs.return()
   }
-
-  useEffect(() => {
-    if ($channel?.source != 'xmtp')
-      return;
-
-    const streamConversations = async () => {
-      const xmtpNew = await authContext?.xmtpClient?.conversations?.stream();
-      console.log("New xmtpConvo ", xmtpNew);
-      setXmtpConvo(xmtpNew);
-    };
-    streamConversations();
-  }, [$channel?.id]);
-
-  useEffect(() => {
-    if (xmtpConvo) {
-      _listen();
-    }
-  }, [xmtpConvo]);
-
-  useEffect(() => {
-    if (allConversations?.length) {
-    }
-  }, [allConversations]);
 
   const _fetch = async () => {
     const conversationList = await authContext?.xmtpClient?.conversations?.list();
     setRawConversations(conversationList);
 
-    // const data = conversationList?.map((item: any) => {
-    //   // const peer = hookLensProfileList.fetch(item.peerAddress);
-    //   // console.log("useXmtpChannels peer", ChannelXMTP$({...item, peer: peer}));
-    //   return new Channel$("xmtp", item);
-    // });
     const conversationMap = await conversationList
       ?.map(async (item: any) => {
         const peer = await hookLensProfileList.fetch(item.peerAddress);
