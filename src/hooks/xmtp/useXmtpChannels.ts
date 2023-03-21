c7a0a97d7f47b20ecfac1e5d9f64b46adc48e7a5
Aditya <aditya26sg@gmail.com>
2023-03-21 12:42:21 +0530

    condition checks

--- src/hooks/xmtp/useXmtpChannels.ts
+++ src/hooks/xmtp/useXmtpChannels.ts
@@ -26,9 +26,15 @@ const useXmtpChannels = () => {
     console.log("Calling useXmtpChannels._watch", authContext?.xmtpLogs);
     for await (const conversation of authContext?.xmtpLogs) {
       console.log("New conversation started with ", conversation);
-      const conversations = [...allConversations];
-      conversations.unshift(new Channel$("xmtp", conversation));
-      setAllConversations(conversations);
+      let conversations: any[] = [];
+      if (allConversations) {
+        conversations = [...allConversations];
+        conversations.unshift(new Channel$("xmtp", conversation));
+        setAllConversations(conversations);
+      } else {
+        console.log("No conversations");
+      }
+        
     }
   };
 
