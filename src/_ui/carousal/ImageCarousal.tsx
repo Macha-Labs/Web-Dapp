4d60d2de2e285deb872f2c1f528d6805c5dcfbf4
aakash-taneja <aakashtaneja12@gmail.com>
2023-05-20 20:30:05 +0530

    carousal

--- src/_ui/carousal/ImageCarousal.tsx
+++ src/_ui/carousal/ImageCarousal.tsx
@@ -1,7 +1,112 @@
-import React from "react";
+import { Box, Flex, IconButton, Image, Text } from "@chakra-ui/react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import { useState } from "react";
+import IconImage from "../icons/IconImage";
+import FlexRow from "../flex/FlexRow";
 
-function ImageCarousal() {
-  return <div>ImageCarousal</div>;
-}
+const carouselData = [
+  {
+    id: 0,
+    coverImage:
+      "https://media.sproutsocial.com/uploads/2021/05/twitter-profile-photo-example.png",
+    profileImage:
+      "https://w7.pngwing.com/pngs/613/636/png-transparent-computer-icons-user-profile-male-avatar-avatar-heroes-logo-black.png",
+    title: "Card 1",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: 1,
+    coverImage:
+      "https://media.sproutsocial.com/uploads/2021/05/twitter-header-photo-1.png",
+    profileImage: "path-to-profile-image-2.jpg",
+    title: "Card 2",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: 3,
+    coverImage:
+      "https://media.sproutsocial.com/uploads/2021/05/twitter-header-photo-1.png",
+    profileImage: "path-to-profile-image-2.jpg",
+    title: "Card 3",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: 4,
+    coverImage:
+      "https://media.sproutsocial.com/uploads/2021/05/twitter-header-photo-1.png",
+    profileImage: "path-to-profile-image-2.jpg",
+    title: "Card 4",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+];
 
-export default ImageCarousal;
+const CardCarousel = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handlePrev = () => {
+    setActiveIndex((prevIndex) =>
+      prevIndex === 0 ? carouselData.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNext = () => {
+    setActiveIndex((prevIndex) =>
+      prevIndex === carouselData.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  return (
+    <Box overflow="hidden">
+      <FlexRow vrAlign="center" hrAlign="center">
+        <IconImage slug="icon-chevron-next" onClick={handlePrev} size="2xl" />
+        <Box
+          display="flex"
+          width="100%"
+          transition="transform 0.3s ease-in-out"
+          transform={`translateX(-${activeIndex * 100}%)`}
+        >
+          {carouselData.map((card, index) => (
+            <Box
+              key={index}
+              flex="0 0 100%"
+              opacity={activeIndex === index ? 1 : 0}
+              transform={`translateX(${(index - activeIndex) * 100}%)`}
+            >
+              <Image src={card.coverImage} alt="Cover Image" w="100%" />
+              <FlexRow margin="10px 0px">
+                <Image
+                  src={card.profileImage}
+                  alt="Profile Image"
+                  w="50px"
+                  // borderRadius="full"
+                  mr={2}
+                />
+                <Text fontWeight="bold" className="m-b-0">
+                  {card.title}
+                </Text>
+              </FlexRow>
+              <Text mt={2}>{card.description}</Text>
+            </Box>
+          ))}
+        </Box>
+        <IconImage slug="icon-chevron-next" onClick={handleNext} size="2xl" />
+      </FlexRow>
+      <FlexRow hrAlign="center" vrAlign="center" width="100%">
+        {carouselData.map((_, index) => (
+          <Box
+            key={index}
+            w="4px"
+            h="4px"
+            borderRadius="full"
+            bg={activeIndex === index ? "gray.400" : "gray.800"}
+            mx={2}
+            onClick={() => setActiveIndex(index)}
+            cursor="pointer"
+          />
+        ))}
+      </FlexRow>
+    </Box>
+  );
+};
+
+export default CardCarousel;
--- src/styles/GlobalIcons.tsx
+++ src/styles/GlobalIcons.tsx
@@ -66,7 +66,7 @@ const GlobalIcons: any = {
     "https://ik.imagekit.io/metaworkLabs/icons/svg/Red_icons/base-red-block.svg?updatedAt=1683699287123",
   "icon-chevron": "/icons/svg/Base-blue/base-blue-chevronBack.svg",
   "icon-chevron-next":
-    "https://ik.imagekit.io/metaworkLabs/icons/png/Base-blue_icons/base-blue-chevronNext.png?updatedAt=1680703639925",
+    "/icons/png/Base-blue_icons/base-blue-chevronNext.png?updatedAt=1680703639925",
   "icon-chevron-down": "/icons/svg/Base-blue/base-blue-chevronDown.svg",
   "icon-reply":
     "https://ik.imagekit.io/metaworkLabs/icons/svg/Base-blue/base-blue-replyFilled.svg",
