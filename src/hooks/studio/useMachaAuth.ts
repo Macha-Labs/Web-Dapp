438d6f4720da7971fea4a0452a75bb3d3328f2fa
Aditya <aditya26sg@gmail.com>
2023-05-22 17:23:38 +0530

    sdk fetching bug solved

--- src/hooks/studio/useMachaAuth.ts
+++ src/hooks/studio/useMachaAuth.ts
@@ -3,8 +3,6 @@ import useUserStore from "@/store/useUserStore";
 import { Macha } from "@metaworklabs/macha-dev-sdk/lib";
 import { useEffect } from "react";
 
-declare let window: any;
-
 const useMachaAuth = () => {
   const $address = useAuthStore((state: any) => state.address);
   const $signer = useAuthStore((state: any) => state.signer);
@@ -20,7 +18,10 @@ const useMachaAuth = () => {
     await macha.connectClient({
       owner: "0x4eff290c1a734411b39aaa96eabe1e25f0e223ae",
     });
-    console.log("Macha init ", macha.client);
+    console.log("Macha init ", macha.client.data);
+    console.log("Macha Client ", macha.client);
+    console.log("Macha Client metasOwned ", macha.client?.metasOwned);
+
     $loadMacha(macha);
   };
 
@@ -30,12 +31,11 @@ const useMachaAuth = () => {
   }, [$address, $signer]);
 
   useEffect(() => {
-    if ($macha?.client?.metasOwned) {
-      console.log("Logging client ", $macha);
-      console.log("Metas Data array ", $macha?.client?.metasOwned?.data);
+    console.log("Logging client ", $macha.client); // getting the right value
+      console.log("Logging client metasOwned", $macha.client?.metasOwned); // coming null
+      console.log("Metas Data array ", $macha?.client?.metasOwned?.data); // coming undefined
       $loadUserMetas($macha?.client?.metasOwned?.data);
-    }
-  }, [$macha?.timestamp]);
+  }, [$macha]);
 
   return {};
 };
