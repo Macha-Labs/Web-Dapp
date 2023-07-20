a58877e3c94b5cdd310cfe07cf37aff9a7d25cf6
Harsh Mathur <harsh@felicity.care>
2023-07-20 19:50:55 +0530

    User Transaction Boilerplate

---
+++ src/hooks/studio/useUserTxn.ts
@@ -0,0 +1,38 @@
+import { txnByUserAddress } from "@/service/ApiService";
+import { useState } from "react";
+
+const useUserTxn = () => {
+
+  const [userTxnDetails, setUserTxnDetails] = useState<any>();
+  const [filteredData, setFilteredData] = useState<any>(userTxnDetails);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [searchVal,setSearchVal] = useState<any>("");
+
+  const _fetch = async (from_address: any) => {
+    txnByUserAddress(from_address).then((res: any) => {
+      console.log("user txn fetching", res);
+      setIsLoading(false)
+      setUserTxnDetails(res.data);
+      setFilteredData(res.data);
+    });
+  };
+
+  const handleFilter = (inputValue: string) => {
+    const filtered = userTxnDetails.filter((item: any) => {
+      return item.transaction.txn_hash.toLowerCase().includes(inputValue.toLowerCase());
+    });
+    setFilteredData(filtered);
+  };
+
+  return {
+    contractTxnDetails: userTxnDetails,
+    filteredData: filteredData,
+    setFilteredData: setFilteredData,
+    handleFilter: handleFilter,
+    searchVal: searchVal,
+    setSearchVal: setSearchVal,
+    isLoading: isLoading,
+    _fetch: _fetch
+  }
+}
+export default useUserTxn
--- src/service/ApiService.tsx
+++ src/service/ApiService.tsx
@@ -73,6 +73,7 @@ export const deleteContract = async (contract_id: any) => {
   console.log("Deleted Contract", response);
   return response.json();
 };
+
 export const txnByChainId = async (chain_id: any) => {
   const response = await fetch(
     `${config.metaServer}/indexer/transactions/fetch-by-chain-id/${chain_id}`
@@ -80,6 +81,15 @@ export const txnByChainId = async (chain_id: any) => {
   const data = await response.json();
   return data;
 };
-// /indexer/transactions/fetch-by-contract-address/:contract_address
+
+export const txnByUserAddress = async (from_address: any) => {
+  const response = await fetch(
+    `${config.metaServer}/indexer/transactions/fetch-by-user-address/${from_address}`
+  );
+  const data = await response.json();
+  return data;
+};
+
+// /indexer/transactions/fetch-by-user-address/:from_address
 
 // /indexer/transactions/fetch-by-chain-id/:chain_id
