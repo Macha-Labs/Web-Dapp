df65755efb45402ebff8717196b41d78b3554eda
aakash-taneja <aakashtaneja12@gmail.com>
2023-07-29 19:38:49 +0530

    mobile state

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,96 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import ButtonNative from "@/_ui/buttons/ButtonNative";
+import FlexRow from "@/_ui/flex/FlexRow";
+import { style } from "@/styles/StyledConstants";
+import { Box, Button, Image, Text } from "@chakra-ui/react";
+import { Html, Head, Main, NextScript } from "next/document";
+import Link from "next/link";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Main />
-        <NextScript />
+        <Box
+          sx={{
+            "@media screen and (min-width: 769px)": { display: "none" },
+          }}
+          bg={"#000"}
+          height={"100vh"}
+          width={"100vw"}
+          display={"flex"}
+          flexDir={"column"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Box
+            display={"flex"}
+            flexDir={"column"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            backgroundImage="url(https://ik.imagekit.io/macha/studio/Studio%20mobile%20empty%20state%20blur.svg)"
+            backgroundRepeat="no-repeat"
+            backgroundSize="contain"
+            backgroundPosition="top center"
+            paddingX={style.padding.sm}
+          >
+            <Text
+              marginBottom={style.margin.xl}
+              fontSize={style.font.h3}
+              textAlign={"center"}
+            >
+              Thanks for visiting!
+            </Text>
+            <Image
+              src="https://ik.imagekit.io/macha/studio/studio%20mobile%20empty%20state.svg?updatedAt=1690639033750"
+              width={"15rem"}
+              marginBottom={style.margin.xxl}
+            />
+            <Text
+              fontSize={style.font.h4}
+              textAlign={"center"}
+              marginBottom={style.margin.xl}
+            >
+              This link is currently only accessible on desktop.
+            </Text>
+            <Text
+              fontSize={style.font.h4}
+              color={style.color["white.5"]}
+              textAlign={"center"}
+              marginBottom={style.margin.xl}
+            >
+              Good news- our mobile app is in the works and will be available
+              soon.
+            </Text>
+            <FlexRow height="fit-content">
+              <Link
+                href={"https://twitter.com/intent/follow?screen_name=Macha0x"}
+              >
+                <Button
+                  style={{
+                    background: `${style.button.bg.active}`,
+                    color: "#FF",
+                  }}
+                  padding={style.padding.sm}
+                  borderRadius={style.button.borderRadius.default}
+                  height={"2rem"}
+                >
+                  Follow On Twitter
+                </Button>
+              </Link>
+            </FlexRow>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            "@media screen and (max-width: 768px)": {
+              display: "none",
+            },
+          }}
+        >
+          <Main />
+          <NextScript />
+        </Box>
       </body>
     </Html>
-  )
+  );
 }
