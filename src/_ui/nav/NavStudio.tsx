369820f6d1d0622ddaea0b315d7a90fa30a082ef
Harsh Mathur <harsh@felicity.care>
2023-08-18 18:07:36 +0530

    search on nav studio

--- src/_ui/nav/NavStudio.tsx
+++ src/_ui/nav/NavStudio.tsx
@@ -5,12 +5,24 @@ import FlexRow from "../flex/FlexRow";
 import NavButton from "@/components/buttons/NavButton";
 import { ConnectWalletButton } from "@/components/ConnectWalletButton";
 import useAuthStore from "@/store/useAuthStore";
+import useSearch from "@/hooks/studio/useSearch";
+import { useRouter } from "next/router";
 
 const NavStudio = () => {
   const $address = useAuthStore((state: any) => state.address);
+  const hookSearch = useSearch()
+  const router = useRouter()
   return (
     <NavTop
-      centerElem={<InputSearch onChange={() => { }} value="" />}
+      centerElem={<InputSearch defaultValue={hookSearch.searchString} value={hookSearch.searchString} onChange={(e: any) => hookSearch.setSearchString(e.target.value)}
+        onKeydown={(e: any) => {
+          if (e.key === 'Enter') {
+            e.preventDefault()
+            router.push(`/search/${hookSearch.searchString}`)
+          }
+        }}
+      />
+      }
       rightElem={
         <FlexRow width="fit-content">
           {$address && <NavButton />}
